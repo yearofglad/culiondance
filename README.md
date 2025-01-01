eb10117a599b57fa3a97396ccbacf5d1f21d4033
Ben Fu <bhf2117@columbia.edu>
2025-01-01 03:47:47 -0500

    yay

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # culiondance
+
+This is a Jekyll SSG redesign of culiondance.org. OG creds go to Emma!
--- _site/assets/js/main.js
+++ _site/assets/js/main.js
@@ -1,3 +1,27 @@
 window.addEventListener('load', (event) => {
     console.log("Page loaded.");
+
+    // const button = document.querySelector('.btn-primary');
+
+    // let hue = 0; 
+    // let animationId;
+
+    // function rainbowHover() {
+    //   hue += 1;
+    //   if (hue > 360) {
+    //     hue = 0;
+    //   }
+    //   button.style.backgroundColor = `hsl(${hue}, 100%, 50%)`;
+    //   button.style.borderColor = `hsl(${hue}, 100%, 50%)`;
+    //   requestAnimationFrame(rainbowHover);
+    //   animationId = requestAnimationFrame(rainbowHover); 
+    // }
+
+    // function stopRainbow() {
+    //   cancelAnimationFrame(animationId); 
+    //   button.style.backgroundColor = 'white';
+    // }
+
+    // button.addEventListener('mouseenter', rainbowHover);
+    // button.addEventListener('mouseleave', stopRainbow); 
   });
